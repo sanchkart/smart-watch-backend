f7579943d5ea0c0b6e6472c668fd8ccb650efb77
Sanchir Kartiev <sanchir.kartiev@gmail.com>
2021-11-21 17:50:06 +0100

    Phase 2 - Build additional infrastructure for microservices

--- backend/src/main/java/com/ing/smartwatch/backend/SmartWatchBackendApplication.java
+++ backend/src/main/java/com/ing/smartwatch/backend/SmartWatchBackendApplication.java
@@ -2,15 +2,11 @@ package com.ing.smartwatch.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @RefreshScope
-@EnableDiscoveryClient
-@EnableFeignClients
 @EnableEurekaClient
 public class SmartWatchBackendApplication {
     public static void main(String[] args) {
